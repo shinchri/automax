254d4ea2913994a45a8a5d4fd11cc6098a9b10e9
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-02 11:15:26 -0500

    feat: Add main template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def main_view(request):
-  return HttpResponse("<h1>Welcome to Automax!</h1>")
+  return render(request, "views/main.html", {"name": "AutoMax"})
