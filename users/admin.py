7e7ccbd42abf8c7569ab2a928bc22d3dff5a66b4
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-04 16:27:26 -0500

    feat: Create Location class to use for Profile

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, Location
 
 # Register your models here.
 class ProfileAdmin(admin.ModelAdmin):
   pass
 
+class LocationAdmin(admin.ModelAdmin):
+  pass
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Location, LocationAdmin)
