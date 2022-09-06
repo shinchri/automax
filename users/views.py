5be95e0968c17ed04673a6e56952beb1831de9cf
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-06 16:14:27 -0500

    feat: Django class view for registeration

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.views import View
 
 # Create your views here.
 def login_view(request):
@@ -27,6 +28,15 @@ def login_view(request):
   return render(request, 'views/login.html', {"login_form": login_form})
 
 
-def register_view(request):
-  register_form = UserCreationForm()
-  return render(request, 'views/register.html', {'register_form': register_form})
+class RegisterView(View):
+
+  def get(self, request):
+    register_form = UserCreationForm()
+    return render(request, 'views/register.html', {'register_form', register_form})
+
+  def post(self, request):
+    pass
+
+# def register_view(request):
+#   register_form = UserCreationForm()
+#   return render(request, 'views/register.html', {'register_form': register_form})
