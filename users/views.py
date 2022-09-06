a4ce19e180c87c081977dc3fbb2258849a3b27df
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-06 16:07:47 -0500

    feat: Create register view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
@@ -25,3 +25,8 @@ def login_view(request):
     login_form = AuthenticationForm()
   
   return render(request, 'views/login.html', {"login_form": login_form})
+
+
+def register_view(request):
+  register_form = UserCreationForm()
+  return render(request, 'views/register.html', {'register_form': register_form})
