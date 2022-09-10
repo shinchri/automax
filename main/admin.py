f309754fb29707570e5e09815c448a2fc11724db
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-10 15:49:41 -0500

    feat: Add more fields to Listing model includigng dropdown; add to admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from main.models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+  pass
+
 # Register your models here.
+admin.site.register(Listing, ListingAdmin)
