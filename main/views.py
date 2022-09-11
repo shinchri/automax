4c8c98033a5bc5853eeabd74ad939939b78ae344
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-11 17:48:28 -0500

    feat: Add section to homepage - rough

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Listing
+
 # Create your views here.
 def main_view(request):
   return render(request, "views/main.html", {"name": "AutoMax"})
 
 @login_required
 def home_view(request):
-  return render(request, "views/home.html")
+  listings = Listing.objects.all()
+  context= {
+    'listings': listings,
+  }
+  return render(request, "views/home.html", context)
