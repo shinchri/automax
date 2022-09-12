b1658b18cbcf245ea08510f98215b494682c3564
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-11 19:52:59 -0500

    feat: Create list view

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home_view, main_view
+from .views import home_view, list_view, main_view
 
 urlpatterns = [
     path('', main_view, name='main'),
     path('home/', home_view, name="home"),
+    path('list/', list_view, name="list"),
 ]
--- main/views.py
+++ main/views.py
@@ -14,3 +14,11 @@ def home_view(request):
     'listings': listings,
   }
   return render(request, "views/home.html", context)
+
+@login_required
+def list_view(request):
+  if request.method == 'POST':
+    pass
+  elif request.method == 'GET':
+    pass
+  return render(request, "views/list.html", {})
