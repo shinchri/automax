21b997e4dd4ff228638c33d6ee77abef2adb2dc1
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-12 13:57:37 -0500

    feat: Django URL parameter for Listing page

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home_view, list_view, main_view
+from .views import home_view, list_view, main_view, listing_view
 
 urlpatterns = [
     path('', main_view, name='main'),
     path('home/', home_view, name="home"),
     path('list/', list_view, name="list"),
+    path('listing/<str:id>/', listing_view, name="listing"),
 ]
