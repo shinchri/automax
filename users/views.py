5ba1f73d49e4d4891c5a94f8e259bee9f19fc36e
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-13 09:35:45 -0500

    feat: Show Liked Listing

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views import View
-from main.models import Listing
+from main.models import Listing, LikedListing
 
 from users.forms import LocationForm, ProfileForm, UserForm
 
@@ -63,7 +63,8 @@ class RegisterView(View):
 class ProfileView(View):
 
   def get(self, request):
-    user_listings = Listing.objects.filter(seller=request.user.profile)
+    user_listings = Listing.objects.filter(seller=request.user.profile).all()
+    user_liked_listings = LikedListing.objects.filter(profile=request.user.profile).all()
     user_form = UserForm(instance=request.user)
     profile_form = ProfileForm(instance=request.user.profile)
     location_form = LocationForm(instance=request.user.profile.location)
@@ -72,11 +73,13 @@ class ProfileView(View):
         {'user_form': user_form, 
         'profile_form': profile_form, 
         'location_form': location_form,
-        'user_listings': user_listings}
+        'user_listings': user_listings,
+        'user_liked_listings': user_liked_listings}
       ) 
 
   def post(self, request):
-    user_listings = Listing.objects.filter(seller=request.user.profile)
+    user_listings = Listing.objects.filter(seller=request.user.profile).all()
+    user_liked_listings = LikedListing.objects.filter(profile=request.user.profile).all()
     user_form = UserForm(request.POST, instance=request.user) # without instance specified, it will create new instance
     profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
     location_form = LocationForm(request.POST, instance=request.user.profile.location)
@@ -93,5 +96,6 @@ class ProfileView(View):
         {'user_form': user_form, 
         'profile_form': profile_form, 
         'location_form': location_form,
-        'user_listings': user_listings}
+        'user_listings': user_listings,
+        'user_liked_listings': user_liked_listings}
       )
