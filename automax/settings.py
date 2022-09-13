8b1f7d9e535dbcc8f9bcdfa4097844b70d4fbc6b
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-13 13:55:47 -0500

    feat: Configure Email Service using Mailgun and Heroku

--- automax/settings.py
+++ automax/settings.py
@@ -150,6 +150,14 @@ MEDIA_URL = '/media/'
 # Django Crispy Form Settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_USER_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
